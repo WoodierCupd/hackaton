de1a4efe375bb3c688d47e92301429301481054c
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2023-01-26 11:57:20 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-2">
@@ -13,7 +7,7 @@
                     <a class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" href="">Create lobby</a>
                 </div>
                 <div class="flex justify-center">
-                    <div id="board" style="width: 600px"></div>
+                    <div id="board" style="width: 700px"></div>
                 </div>
             </div>
         </div>
