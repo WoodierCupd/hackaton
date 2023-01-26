1f27484866c06068a3bf025216f14b2b06ad2eb7
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2023-01-26 15:52:00 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mt-2">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <a class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" href="">Join lobby</a>
-                    <a class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" href="">Create lobby</a>
-                </div>
+{{--                <div class="p-6 text-gray-900 dark:text-gray-100">--}}
+{{--                    <a class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" href="">Join lobby</a>--}}
+{{--                    <a class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" href="">Create lobby</a>--}}
+{{--                </div>--}}
                 <div class="status">
                     <h1 class="dark:text-white" id="status"></h1>
                 </div>
