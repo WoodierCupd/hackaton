bd80bc5edbe324f18d54228e03b9b1d104881310
Fonsdoggen <70898032+Fonsdoggen@users.noreply.github.com>
2023-01-26 15:47:58 +0100

    favicon en logo toegevoegd

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,9 +11,9 @@
     <div class="mx-auto max-w-7xl px-6">
         <div class="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
         <div class="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <a href="{{route('views.welcome')}}">
             <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+            <img class="h-8 w-auto sm:h-10" src="{{asset('img/logo.png')}}" alt="">
             </a>
         </div>
         <div class="-my-2 -mr-2 md:hidden">
@@ -236,8 +236,8 @@
             </div>
         </nav>
         <div class="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-            <a href="#" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
-            <a href="#" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
+            <a href="{{route('login')}}" class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
+            <a href="{{route('register')}}" class="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
         </div>
         </div>
     </div>
@@ -328,10 +328,10 @@
             <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700">Security</a>
             </div>
             <div>
-            <a href="#" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
+            <a href="{{route('register')}}" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
             <p class="mt-6 text-center text-base font-medium text-gray-500">
                 Existing customer?
-                <a href="#" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
+                <a href="{{route('login')}}" class="text-indigo-600 hover:text-indigo-500">Sign in</a>
             </p>
             </div>
         </div>
