342bf4acc146fd3801af8eae09103363171beda9
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2023-01-26 15:00:06 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                     <a class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" href="">Create lobby</a>
                 </div>
                 <div class="status">
-                    <h1 id="status"></h1>
+                    <h1 class="dark:text-white" id="status"></h1>
                 </div>
                 <div class="flex justify-center">
                     <div id="board" style="width: 700px"></div>
